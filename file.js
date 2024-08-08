5a8a4af3bb7c6e04898d38267630685d92ffa651
malaikaimran22 <malaikaimra683@gmail.com>
2024-08-08 15:20:00 +0500

    first commit

---
+++ file.js
@@ -0,0 +1,47 @@
+document.getElementById('formvalidation').addEventListener('submit',function(event){
+
+ document.getElementById('usernameError').textContent = '';
+ document.getElementById('emailError').textContent = '';
+ document.getElementById('passwordError').textContent ='';
+ document.getElementById('confirmError').textContent = '';
+
+
+ let isValid = true;
+
+let username = document.getElementById('username').value.trim();
+let email = document.getElementById('email').value.trim();
+let password = document.getElementById('password').value;
+let confirm = document.getElementById('confirm').value;
+
+
+
+if (username === ""){
+    document.getElementById('usernameError').textContent = " username is required";
+ isValid = false;
+}
+
+
+let emailpattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+if( email === "" || !emailpattern.test(email)){
+    document.getElementById('emailError').textContent = "email is not valid"
+    isValid = false;
+}
+
+if (password.length < 6 ){
+    document.getElementById('passwordError').textContent = "password must me at least 6 characters long"
+isValid = false;
+}
+
+if (password !== confirm) {
+    document.getElementById('confirmError').textContent = 'Passwords do not match.';
+    isValid = false;
+}
+
+if(!isValid){
+    event.preventDefault();
+}
+
+
+});
+
+
